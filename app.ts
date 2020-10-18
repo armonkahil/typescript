ce897908cca4727c9ccfab4d963a0ce5d8fcc750
armonkahil <armonkahil@gmail.com>
2020-10-17 21:16:40 -0400

    array types

--- app.ts
+++ app.ts
@@ -1,9 +1,17 @@
-const person: {
-  name: string
-  age: number
-} = {
+// const person: {
+//   name: string
+//   age: number
+// } = {
+const person = {
   name: 'Maximilian',
-  age: 30
+  age: 30,
+  hobbies: ['Sports', 'Cooking']
 }
-
+let favoriteActivities: string[]
+favoriteActivities = ["Football"]
 console.log(person.name)
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase())
+
+}
