c12f62fbf8b4815329b15fae84668b573a0e9344
armonkahil <armonkahil@gmail.com>
2020-10-17 22:24:33 -0400

    Union Types

--- app.js
+++ app.js
@@ -1,38 +1,14 @@
-// const person: {
-//   name: string
-//   age: number
-// // } = {
-// const person: {
-//   // string
-//   name: string
-//   // number
-//   age: number
-//   // array
-//   hobbies: string[]
-//   // tuple
-//   role: [number, string]
-var ADMIN = 0;
-var READ_ONLY = 1;
-var AUTHOR = 2;
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 0] = "ADMIN";
-    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 2] = "AUTHOR";
-})(Role || (Role = {}));
-var person = {
-    name: 'Maximilian',
-    age: 30,
-    hobbies: ['Sports', 'Cooking'],
-    role: Role.ADMIN
+var combine = function (input1, input2) {
+    var result;
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + " and " + input2.toString();
+    }
+    return result;
 };
-var favoriteActivities;
-favoriteActivities = ["Football"];
-console.log(person.name);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
-}
-if (person.role === Role.ADMIN) {
-    console.log('is admin');
-}
+var combinedAges = combine(30, 26);
+console.log('combinedAges:', combinedAges);
+var combinedNames = combine('Armon', 'Amy');
+console.log('combinedNames:', combinedNames);
---
+++ coreTypes/any.ts
@@ -0,0 +1,40 @@
+// const person: {
+//   name: string
+//   age: number
+// // } = {
+// const person: {
+//   // string
+//   name: string
+//   // number
+//   age: number
+//   // array
+//   hobbies: string[]
+//   // tuple
+//   role: [number, string]
+const ADMIN = 0
+const READ_ONLY = 1
+const AUTHOR = 2
+
+// ENUMS ARE USED FOR more readable identifiers
+// enum Role { ADMIN, READ_ONLY, AUTHOR }
+enum Role { ADMIN = "ADMIN", READ_ONLY = 100, AUTHOR='AUTHOR' }
+
+
+const person = {
+  name: 'Maximilian',
+  age: 30,
+  hobbies: ['Sports', 'Cooking'],
+  role: Role.ADMIN
+}
+
+// Avoid using "any" type as little as possible. 
+let favoriteActivities: any[]
+favoriteActivities = ["Football"]
+console.log(person.name)
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase())
+}
+
+if (person.role === Role.ADMIN) {
+  console.log('is admin')
+}
