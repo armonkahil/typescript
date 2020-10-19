1a4052f6f8ffd3f58df0386873eeb03053e0bfa4
armonkahil <armonkahil@gmail.com>
2020-10-18 21:54:10 -0400

    modules

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-var userInput;
-var userName;
-userInput = 5;
-userInput = 'Max';
-// unknown is not the same as any
-// Typescript detects the type check. 
-if (typeof userInput === 'string') {
-    userName = userInput;
-}
-var generateError = function (message, code) {
-    throw { message: message, errorCode: code };
-};
-var result = generateError('An error has occurred', 500);
-console.log(result);
+"use strict";
+var userName = 'Armon';
+console.log(userName);
--- app.ts
+++ app.ts
@@ -1,17 +1,2 @@
-let userInput: unknown
-let userName: string
-userInput = 5
-userInput = 'Max'
-// unknown is not the same as any
-// Typescript detects the type check. 
-if (typeof userInput === 'string') {
-  userName = userInput
-}
-
-
-const generateError = (message: string, code: number): never => {
-  throw {message: message, errorCode: code}
-}
-
-const result = generateError('An error has occurred', 500)
-console.log(result)
+const userName: string = 'Armon'
+console.log(userName)
